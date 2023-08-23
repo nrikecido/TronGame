58ce8263da0d47df6f57473d89a2273c143d8768
Lenovo\Usuario <juguera_bellus@hotmail.com>
2023-08-23 18:59:08 +0200

    Working in keyboard

--- TronGame.js
+++ TronGame.js
@@ -46,9 +46,81 @@ class TronGame{
         this.context.fillStyle = `rgb(${this.player2.color.r}, ${this.player2.color.g}, ${this.player2.color.b})`;
         this.context.fillRect(this.player2.x, this.player2.y, this.player2.width, this.player2.height);
     }
+
+    move_player1(){
+        const position = this.player1;
+
+    }
+}
+
+class KeyBoard{
+    constructor(game){
+        this.game = tron;
+
+        this.keyMapPlayer1 = {};
+        this.keyMapPlayer2 = {};
+
+        // Asigna los eventos de teclado
+        document.addEventListener('keydown', (event) => this.handleKeyDown(event));
+        document.addEventListener('keyup', (event) => this.handleKeyUp(event));
+
+    }
+
+    handleKeyDown(event) {
+        // Jugador 1
+        if (event.key === 'ArrowUp' && !this.keyMapPlayer1['ArrowDown']) {
+            this.keyMapPlayer1['ArrowUp'] = true;
+            console.log('estoy presionando flecha arriba');
+        } else if (event.key === 'ArrowDown' && !this.keyMapPlayer1['ArrowUp']) {
+            this.keyMapPlayer1['ArrowDown'] = true;
+        } else if (event.key === 'ArrowLeft' && !this.keyMapPlayer1['ArrowRight']) {
+            this.keyMapPlayer1['ArrowLeft'] = true;
+        } else if (event.key === 'ArrowRight' && !this.keyMapPlayer1['ArrowLeft']) {
+            this.keyMapPlayer1['ArrowRight'] = true;
+        }
+
+        // Jugador 2
+        if (event.key === 'W' && !this.keyMapPlayer2['S']) {
+            this.keyMapPlayer2['W'] = true;
+            console.log('soy el w del jugador dos');
+        } else if (event.key === 'S' && !this.keyMapPlayer2['W']) {
+            this.keyMapPlayer2['S'] = true;
+        } else if (event.key === 'A' && !this.keyMapPlayer2['D']) {
+            this.keyMapPlayer2['A'] = true;
+        } else if (event.key === 'D' && !this.keyMapPlayer2['A']) {
+            this.keyMapPlayer2['D'] = true;
+        }
+    }
+
+    handleKeyUp(event) {
+        // Jugador 1
+        if (event.key === 'ArrowUp') {
+            this.keyMapPlayer1['ArrowUp'] = false;
+            console.log('estoy soltando flecha arriba');
+        } else if (event.key === 'ArrowDown') {
+            this.keyMapPlayer1['ArrowDown'] = false;
+        } else if (event.key === 'ArrowLeft') {
+            this.keyMapPlayer1['ArrowLeft'] = false;
+        } else if (event.key === 'ArrowRight') {
+            this.keyMapPlayer1['ArrowRight'] = false;
+        }
+
+        // Jugador 2
+        if (event.key === 'W') {
+            this.keyMapPlayer2['W'] = false;
+            console.log('soy el w del jugador dos');
+        } else if (event.key === 'S') {
+            this.keyMapPlayer2['S'] = false;
+        } else if (event.key === 'A') {
+            this.keyMapPlayer2['A'] = false;
+        } else if (event.key === 'D') {
+            this.keyMapPlayer2['D'] = false;
+        }
+    }
 }
 
 window.onload = () => {
     tron = new TronGame();
     tron.load_track_player();
+    teclado = new KeyBoard();
 };
